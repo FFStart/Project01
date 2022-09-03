8281476cd35392da574d8416959ea01cac088249
FFStart <610322225@qq.com>
2022-09-03 20:12:54 +0800

    Revert: update

--- Module01/src/com/study/test/HelloWorld.java
+++ Module01/src/com/study/test/HelloWorld.java
@@ -5,10 +5,5 @@ public class HelloWorld {
     public HelloWorld() {
     }
 
-    public static void main(String[] args) {
-        System.out.println("sss");
-        System.out.println("sss");
-    }
-
 
 }
